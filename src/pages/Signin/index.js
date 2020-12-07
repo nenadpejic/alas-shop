30c68615a1462e201652b5d778be649a0090d595
Saiyanffs <damjan.zugic94@gmail.com>
2020-12-07 17:03:41 +0100

    Signin scss style

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import './style.scss';
 
 const Signin = () => {
@@ -13,27 +13,23 @@ const Signin = () => {
     setPassword("");
   }
 
-  return ( 
+  return (
     <main className="signin">
       <div className="wrapper">
         <h1>Sign in</h1>
         <form className="signin-form" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="signin-form-email">Email</label>
-            <input id="signin-form-email" type="email" placeholder="Enter email.."
-            onChange={(e) => setEmail(e.target.value)} value={email}/>
-          </div>
-          <div>
-            <label htmlFor="signin-form-password">Password</label>
-            <input id="signin-form-password" type="password" placeholder="Enter password.."
-            onChange={(e) => setPassword(e.target.value)} value={password}/>
-          </div>
-          <button type="submit">Sign in</button>
+          <label htmlFor="signin-form-email">Email</label>
+          <input id="signin-form-email" type="email" placeholder="Enter email.."
+            onChange={(e) => setEmail(e.target.value)} value={email} />
+          <label htmlFor="signin-form-password">Password</label>
+          <input id="signin-form-password" type="password" placeholder="Enter password.."
+            onChange={(e) => setPassword(e.target.value)} value={password} />
+          <button className="submitBtn" type="submit">Sign in</button>
         </form>
-        <p>Need an account? <Link to="/signup">Sign up</Link></p>
+        <p className="existingAcc">Need an account? <Link to="/signup">Sign up</Link></p>
       </div>
     </main>
-   );
+  );
 }
- 
+
 export default Signin;
