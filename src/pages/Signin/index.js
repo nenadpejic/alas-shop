09b42531908a9df2ab82fccae1d6a1f816633f56
nenadpejic <npejic89@gmail.com>
2020-12-07 18:37:00 +0100

    Added authentication

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -2,16 +2,32 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import './style.scss';
 import logo from "../../assets/logo.png";
+import {auth} from "../../services/fire";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    setEmail("");
-    setPassword("");
+    auth.signInWithEmailAndPassword(email, password)
+      .then(res => {
+        // console.log(res);
+      })
+      .catch(err => {
+        if (err.code === "auth/invalid-email") {
+          setErrMsg("Invalid email address.")
+        } else if (err.code === "auth/user-disabled") {
+          setErrMsg("This account has been disabled.")
+        } else if (err.code === "auth/user-not-found") {
+          setErrMsg("There is no account with this email.")
+        } else if (err.code === "auth/wrong-password") {
+          setErrMsg("Wrong password.")
+        };
+        setEmail("");
+        setPassword("");
+      });
   }
 
   return (
@@ -29,6 +45,7 @@ const Signin = () => {
           <input id="signin-form-password" type="password" placeholder="Enter password.."
             onChange={(e) => setPassword(e.target.value)} value={password} />
           <button className="submitBtn" type="submit">Sign in</button>
+          <p>{errMsg}</p>
         </form>
         <p className="existingAcc">Need an account? <Link to="/signup">Sign up</Link></p>
       </div>
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -8,21 +8,38 @@ const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setErrMsg("");
 
-    auth.createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-    setEmail("");
-    setPassword("");
-    setConfirmPassword("");
+    if (password !== confirmPassword) {
+      setErrMsg("Passwords do not match.");
+    } else if (/\s/.test(email)) {
+      setErrMsg("Email contains whitespaces");
+    } else if (/\s/.test(password)) {
+      setErrMsg("Password contains whitespaces");
+    } else {
+      auth.createUserWithEmailAndPassword(email, password)
+        .then(res => {
+          // console.log(res);
+        })
+        .catch(err => {
+          if (err.code === "auth/email-already-in-use") {
+            setErrMsg("There already exists an account with that email.")
+          } else if (err.code === "auth/invalid-email") {
+            setErrMsg("Invalid email address.")
+          } else if (err.code === "auth/operation-not-allowed") {
+            setErrMsg("Email/password accounts are not enabled.")
+          } else if (err.code === "auth/weak-password") {
+            setErrMsg("Password must be at least 6 characters long.")
+          };
+        });
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+    };
   }
 
   return (
@@ -34,15 +51,16 @@ const Signup = () => {
         <h1>Sign up</h1>
         <form className="signup-form" onSubmit={handleSubmit}>
           <label htmlFor="signup-form-email">Email</label>
-          <input id="signup-form-email" type="email" placeholder="Enter email.."
+          <input id="signup-form-email" type="email" placeholder="Enter email.." required
             onChange={(e) => setEmail(e.target.value)} value={email} />
           <label htmlFor="signup-form-password">Password</label>
-          <input id="signup-form-password" type="password" placeholder="Enter password.."
+          <input id="signup-form-password" type="password" placeholder="Enter password.." required
             onChange={(e) => setPassword(e.target.value)} value={password} />
           <label htmlFor="signup-form-confirm-password">Confirm Password</label>
-          <input id="signup-form-confirm-password" type="password" placeholder="Confirm password.."
+          <input id="signup-form-confirm-password" type="password" placeholder="Confirm password.." required
             onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
           <button className="submitBtn" type="submit">Sign up</button>
+          <p>{errMsg}</p>
         </form>
         <p className="existingAcc">Already have an account?<Link to="/signin">Sign in</Link></p>
       </div>
