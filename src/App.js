b0088570dabafa6631fd3e6623e55db191953967
dzalee16 <mladen.jovic93@yahoo.com>
2020-12-07 11:15:09 +0100

    Create Route in App.js, and install react-router-dom, all route are under comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.scss';
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <div>APP</div> //Ovo se brise
+
+    // <div className="app">
+    //   <Router>
+    //     <Route exact path="/">
+    //       <Landing />
+    //     </Route>
+    //     <Route path="/singup">
+    //       <Singup />
+    //     </Route>
+    //     <Route path="/singin">
+    //       <Singin />
+    //     </Route>
+    //     <Route path="/home">
+    //       <Home />
+    //     </Route>
+    //     <Route path="/history">
+    //       <History />
+    //     </Route>
+    //   </Router>
+    // </div>
   );
-}
+};
 
 export default App;
