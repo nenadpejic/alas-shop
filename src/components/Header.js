748476372a0630954f214ff6817593257d10129e
Saiyanffs <damjan.zugic94@gmail.com>
2020-12-07 14:24:06 +0100

    added small scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import './Header.scss';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="wrapper">
         <Link to="/">
-          <img src={Logo} alt="logo" />
+          <img src={Logo} alt="logo" className="logo" />
         </Link>
-        <div className="header-list">
-          <ul>
-            <li>
-              <Link to="/singin">Sing up</Link>
-            </li>
-            <li>
-              <Link to="/singup">Sing in</Link>
-            </li>
-          </ul>
-        </div>
+        {/* <div className="header-list"> */}
+        <ul className="nav">
+          <li>
+            <Link to="/singin">Sing up</Link>
+          </li>
+          <li>
+            <Link to="/singup">Sing in</Link>
+          </li>
+        </ul>
+        {/* </div> */}
       </div>
     </header>
   );
