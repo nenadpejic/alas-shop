9f46abaf28dbad90d457d6795a7fef75d80d31c3
dzalee16 <mladen.jovic93@yahoo.com>
2020-12-07 13:29:04 +0100

    Add image and className wrapper in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header-logo">
-        <Link to="/">Logo</Link>
+    <header className="header">
+      <div className="wrapper">
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
+        <div className="header-list">
+          <ul>
+            <li>
+              <Link to="/singin">Sing up</Link>
+            </li>
+            <li>
+              <Link to="/singup">Sing in</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="header-list">
-        <ul>
-          <li>
-            <Link to="/singin">Sing up</Link>
-          </li>
-          <li>
-            <Link to="/singup">Sing in</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </header>
   );
 };
 
