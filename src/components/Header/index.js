c774dc1b3e43980aef59bb62918be87f5d990b46
nenadpejic <npejic89@gmail.com>
2020-12-08 13:04:40 +0100

    Saved changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
-<<<<<<< HEAD
 import './style.scss';
 import {auth} from "../../services/fire";
 import {UserContext} from "../../contexts/UserContext";
@@ -9,12 +8,6 @@ import {UserContext} from "../../contexts/UserContext";
 const Header = () => {
   const user = useContext(UserContext);
 
-=======
-import "./style.scss";
-import { auth } from "../../services/fire";
-
-const Header = () => {
->>>>>>> 49c01cb3db465b0d8809b4fffdb83aaf764034c2
   const handleSignout = () => {
     auth.signOut();
   };
@@ -22,7 +15,6 @@ const Header = () => {
   return (
     <header className="header">
       <div className="wrapper">
-<<<<<<< HEAD
         {user
         ? <>
           <Link to="/home">
@@ -50,22 +42,6 @@ const Header = () => {
             </ul>
           </nav>
         </>}
-=======
-        <Link to="/" className="logoLink">
-          <img src={Logo} alt="logo" className="logo" />
-        </Link>
-        <ul className="nav">
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-          <li>
-            <Link to="/signin">Sign in</Link>
-          </li>
-          <li>
-            <button onClick={handleSignout}>Sign out</button>
-          </li>
-        </ul>
->>>>>>> 49c01cb3db465b0d8809b4fffdb83aaf764034c2
       </div>
     </header>
   );
