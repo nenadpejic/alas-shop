c5a097358d34f801a3d628f1a65848a759c1493c
nenadpejic <npejic89@gmail.com>
2020-12-12 16:21:55 +0100

    PageNotFound main div set to main

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div id="page-not-found">
+    <main id="page-not-found">
       <div className="wrapper">
         <h1>Page Not Found</h1>
         <Link to='/home'>Return to safety</Link>
       </div>
-    </div>
+    </main>
   );
 }
 
