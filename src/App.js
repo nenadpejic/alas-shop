727d5c94cda48ff547075155eae60ca801fa427b
nenadpejic <npejic89@gmail.com>
2020-12-13 14:58:41 +0100

    Patched prev merge

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import UserContextProvider from "./contexts/UserContext";
+import PublicRoute from "./routes/PublicRoute";
+import PrivateRoute from "./routes/PrivateRoute";
+import AdminRoute from "./routes/AdminRoute";
+import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
-// import Admin from "./pages/Admin";
-import "./App.scss";
-import UserContextProvider from "./contexts/UserContext";
-import PublicRoute from "./routes/PublicRoute";
-import PrivateRoute from "./routes/PrivateRoute";
-import AdminRoute from "./routes/AdminRoute";
+import Admin from "./pages/Admin";
 import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
   return (
     <UserContextProvider>
       <Router>
-<<<<<<< HEAD
-        <PublicRoute exact path="/">
-          <Header />
-          <Landing />
-          <Footer />
-        </PublicRoute>
-
-        <PublicRoute path="/signup">
-          <Signup />
-        </PublicRoute>
-
-        <PublicRoute path="/signin">
-          <Signin />
-        </PublicRoute>
-
-        <PrivateRoute path="/home">
-          <Home />
-        </PrivateRoute>
-
-        <PrivateRoute path="/history">
-          {/* <History /> */}
-        </PrivateRoute>
-
-        {/* <AdminRoute path="/admin">
-          <Admin />
-        </AdminRoute> */}
-
-        <Route path='*'>
-          <PageNotFound />
-        </Route>
-=======
         <Switch>
           <PublicRoute exact path="/">
             <Header />
@@ -55,23 +24,23 @@ const App = () => {
             <Footer />
           </PublicRoute>
 
-          <PublicRoute path="/signup">
+          <PublicRoute exact path="/signup">
             <Signup />
           </PublicRoute>
 
-          <PublicRoute path="/signin">
+          <PublicRoute exact path="/signin">
             <Signin />
           </PublicRoute>
 
-          <PrivateRoute path="/home">
+          <PrivateRoute exact path="/home">
             <Home />
           </PrivateRoute>
 
-          <PrivateRoute path="/history">
+          <PrivateRoute exact path="/history">
             {/* <History /> */}
           </PrivateRoute>
 
-          <AdminRoute path="/admin">
+          <AdminRoute exact path="/admin">
             <Admin />
           </AdminRoute>
 
@@ -79,7 +48,6 @@ const App = () => {
             <PageNotFound />
           </Route>
         </Switch>
->>>>>>> 8bb2681fce0fd22e404357bd5595c8ab9fd8e3c0
       </Router>
     </UserContextProvider>
   );
--- src/components/Header/Menu/index.js
+++ src/components/Header/Menu/index.js
@@ -14,25 +14,19 @@ const Menu = ({ setMenu }) => {
   }
 
   return (
-    // <ul className='drop-down-menu'>
-    //   <li className="exit-drop-down"><span onClick={() => { setMenu(false) }}>X</span> <span>Account</span></li>
-    //   <li>
-    //     <p>{username}</p>
-    //     <p>{user.email}</p>
-    //     {user.admin && <li className="admin-list-item"><Link className="admin" to="/admin">Admin</Link></li>}
-    //   </li>
-    //   <li onClick={handleSignOut}>Sign out</li>
-    // </ul>
     <ul className="drop-down-menu">
-    <li className="exit-LI"><span onClick={() => { setMenu(false) }}>X</span><span>Account</span></li>
-    <li className="information-LI">
-      <p>{username}</p>
-      <p>{user.email}</p>
-      {user.admin && <p>Admin</p>}
-    </li>
-    {user.admin && <li className="admin-LI"><Link className="admin" to="/admin">Admin</Link></li>}
-    <li onClick={handleSignOut}>Sign out</li>
-  </ul>
+      <li className="exit-LI">
+        <span onClick={() => { setMenu(false) }}>X</span>
+        <span>Account</span>
+      </li>
+      <li className="information-LI">
+        <p>{username}</p>
+        <p>{user.email}</p>
+        {user.admin && <p>Admin</p>}
+      </li>
+      {user.admin && <li className="admin-LI"><Link className="admin" to="/admin">Admin</Link></li>}
+      <li onClick={handleSignOut}>Sign out</li>
+    </ul>
   );
 }
 
