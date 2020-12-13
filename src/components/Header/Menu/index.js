947de7e156e9af0d83a86a88bad5b7b6c067adc4
Saiyanffs <damjan.zugic94@gmail.com>
2020-12-13 05:00:35 -0800

    Styled drop-down

--- src/components/Header/Menu/index.js
+++ src/components/Header/Menu/index.js
@@ -14,17 +14,25 @@ const Menu = ({ setMenu }) => {
   }
 
   return (
-    <ul className='drop-down-menu'>
-      <li className="exit-drop-down"><span onClick={() => { setMenu(false) }}>X</span> <span>Account</span></li>
-      <li>
-        {username}
-      </li>
-      <li>
-        {user.email}
-      </li>
-      {user.admin && <li><Link to="/admin">Admin</Link></li>}
-      <li onClick={handleSignOut}>Sign out</li>
-    </ul>
+    // <ul className='drop-down-menu'>
+    //   <li className="exit-drop-down"><span onClick={() => { setMenu(false) }}>X</span> <span>Account</span></li>
+    //   <li>
+    //     <p>{username}</p>
+    //     <p>{user.email}</p>
+    //     {user.admin && <li className="admin-list-item"><Link className="admin" to="/admin">Admin</Link></li>}
+    //   </li>
+    //   <li onClick={handleSignOut}>Sign out</li>
+    // </ul>
+    <ul className="drop-down-menu">
+    <li className="exit-LI"><span onClick={() => { setMenu(false) }}>X</span><span>Account</span></li>
+    <li className="information-LI">
+      <p>{username}</p>
+      <p>{user.email}</p>
+      {user.admin && <p>Admin</p>}
+    </li>
+    {user.admin && <li className="admin-LI"><Link className="admin" to="/admin">Admin</Link></li>}
+    <li onClick={handleSignOut}>Sign out</li>
+  </ul>
   );
 }
 
